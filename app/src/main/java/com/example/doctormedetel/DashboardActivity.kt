8db35eb960d0b3e163f1551348401e89eee32f0b
Ansuman <ansumangouda72@gmail.com>
2024-06-27 17:33:16 +0530

    hn

--- app/src/main/java/com/example/doctormedetel/DashboardActivity.kt
+++ app/src/main/java/com/example/doctormedetel/DashboardActivity.kt
@@ -1,18 +1,23 @@
 package com.example.doctormedetel
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.transition.FragmentTransitionSupport
 import com.example.doctormedetel.shared_preferences.SharedPreferences
 
 class DashboardActivity : AppCompatActivity() {
     lateinit var text : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+       // enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard)
        /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -26,4 +31,27 @@ class DashboardActivity : AppCompatActivity() {
 
     }
 
+    @SuppressLint("RestrictedApi", "CommitTransaction")
+    private fun lodeFragment(fragment: Fragment, canAddBackStark : Boolean){
+        var fragmentManager:FragmentManager = supportFragmentManager
+        var fragmentTransition: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.fragmentContainer,fragment)
+
+        if (canAddBackStark){
+            fragmentTransition.addToBackStack(fragment.tag)
+
+        }
+        fragmentTransition.commit()
+    }
+
+    private fun setFindViewIds(){
+
+    }
+
 }
+
+
+
+
+
+
